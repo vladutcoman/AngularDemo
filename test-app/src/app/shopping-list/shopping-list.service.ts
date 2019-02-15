187f0148f06e40cf34edda3e867908824b3e1d2e
acoman <andrei-vladut.coman@ionos.com>
2019-02-15 11:02:34 +0200

    observable finish

--- test-app/src/app/shopping-list/shopping-list.service.ts
+++ test-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,5 +1,7 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 export class ShoppingListService {
+  ingredientChange = new Subject<Ingredient[]>();
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tommatoes', 15),
@@ -11,5 +13,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientChange.next(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientChange.next(this.ingredients.slice());
   }
 }
