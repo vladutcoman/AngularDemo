252d7eb3d1e46eefdccbc24012cdc76228645751
unknown <vladut4.95@gmail.com>
2019-03-04 08:21:58 +0200

    recipes module

---
+++ test-app/src/app/recipes/recipes.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "../../../node_modules/@angular/core";
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesComponent } from "./recipes.component";
+import { CommonModule } from "../../../node_modules/@angular/common";
+import { ReactiveFormsModule } from "../../../node_modules/@angular/forms";
+import { RecipesRoutingModule } from "./recipes-routing.module";
+
+@NgModule({
+  declarations: [
+    RecipeDetailComponent,
+    RecipeEditComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    RecipeStartComponent,
+    RecipesComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RecipesRoutingModule
+  ]
+})
+export class RecipesModule {}
