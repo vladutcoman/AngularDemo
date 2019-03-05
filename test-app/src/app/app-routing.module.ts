069a5c8291b051470637251b548ee64587c764ac
unknown <vladut4.95@gmail.com>
2019-03-05 08:01:57 +0200

    start lazy loading

--- test-app/src/app/app-routing.module.ts
+++ test-app/src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '../../node_modules/@angular/core';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent }
